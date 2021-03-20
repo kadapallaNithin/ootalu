87bc0741237e9cf41734ab30c8cd5a22159bdb14
nithin <kadapallanithin@gmail.com>
2021-03-20 16:35:02 +0530

    added db

--- ootalu/settings.py
+++ ootalu/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -150,3 +151,5 @@ EMAIL_HOST_USER = os.environ.get('OOTALU_EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('OOTALU_EMAIL_PASS')
 
 FIRMWARE_UPDATE_PATH = os.path.join(os.path.join(BASE_DIR,'product'),'updates')
+
+django_heroku.settings(locals())
